61cf6ce5ef6776021d3b78ef8072489a031643ce
Ruba <s12028675@stu.najah.edu>
2025-02-19 01:43:01 +0200

    Implemen Edit, Delete, Search Product

--- ProductManagement/ProductController.cs
+++ ProductManagement/ProductController.cs
@@ -21,7 +21,21 @@ namespace INVMS.ProductManagement
             products.Add(product);
             Console.WriteLine($"*** Product {product.Name} Added Successfully ***");
 
-       
+        }
+        public void RemoveProduct(string productName)
+        {
+            var product = GetProduct(productName);
+            if (product != null)
+            {
+                products.Remove(product);
+                Console.WriteLine($"Product {productName} Deleted Successfully");
+            }
+            else
+            {
+                Console.WriteLine($"Delete failed; product {productName} not exist");
+
+            }
+        }
         public Product? GetProduct(string productName)
         {
             foreach (var product in products)
@@ -31,7 +45,63 @@ namespace INVMS.ProductManagement
 
             return null;
         }
-      
+        public void EditProduct(Product product)
+        {
+            Console.WriteLine("************************");
+            Console.WriteLine("1: Edit product name");
+            Console.WriteLine("2: Edit product price");
+            Console.WriteLine("3: Edit product quantity");
+            Console.WriteLine("4: Back to main menu");
+
+            string? userSelection = Console.ReadLine();
+            switch (userSelection)
+            {
+                case "1":
+                    string newName = ReadProductName();
+                    product.Name = newName;
+                    break;
+
+                case "2":
+                    product.Price = Utilities<double>.GetNumber("Enter the product price", double.TryParse);
+                    break;
+
+                case "3":
+                    product.Quantity = Utilities<int>.GetNumber("Enter the product quantity", int.TryParse);
+                    break;
+                case "4":
+                    return;
+
+                default:
+                    Console.WriteLine("!!! Enter a Valid Input !!!");
+                    break;
+            }
+
+        }
+        
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+        private string ReadProductName()
+        {
+            bool b = false;
+            string? name;
+            do
+            {
+                Console.WriteLine("Enter the product name");
+                name = Console.ReadLine();
+                if (GetProduct(name) != null)
+                {
+                    Console.WriteLine("!!! Product name is already exist in inventory !!!");
+                }
+                else
+                    b = true;
+
+            } while (!b);
+            return name;
+
+        }
+
 
     }
 }
