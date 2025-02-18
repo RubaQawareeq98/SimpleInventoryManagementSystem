f48dd0b0c49b53062af4603b4200be9e70a2caac
Ruba <s12028675@stu.najah.edu>
2025-02-19 01:39:05 +0200

    Implement 'Add Product' Feature

---
+++ InventoryManagement/Inventory.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using INVMS.ProductManagement;
+
+namespace INVMS.InventoryManagement
+{
+    internal class Inventory
+    {
+        private IProductFunctionality? _productFunctionality;
+        public Inventory(IProductFunctionality productFunctionality)
+        {
+            _productFunctionality = productFunctionality;
+
+        }
+
+        public void AddProduct(string name, int quantity, double price)
+        {
+            if (_productFunctionality.GetProduct(name) == null)
+            {
+                Product product = new(name, quantity, price);
+                _productFunctionality.AddProduct(product);
+            }
+            else
+            {
+                Console.WriteLine($"Product {name} already exist on the inventory");
+            }
+        }
+        public void RemoveProduct(string name)
+        {
+            _productFunctionality.RemoveProduct(name);
+        }
+        public void EditProduct(Product product)
+        {
+            _productFunctionality.EditProduct(product);
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            return _productFunctionality.GetAllProducts();
+        }
+        public Product? GetProduct(string name)
+        {
+           return _productFunctionality.GetProduct(name);
+        }
+
+    }
+}
---
+++ ProductManagement/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace INVMS.ProductManagement
+{
+    internal class ProductController : IProductFunctionality
+    {
+        private List<Product> products;
+
+        public ProductController() 
+        { 
+            products = new List<Product>();
+        }
+
+        public void AddProduct(Product product)
+        {
+            products.Add(product);
+            Console.WriteLine($"*** Product {product.Name} Added Successfully ***");
+
+       
+        public Product? GetProduct(string productName)
+        {
+            foreach (var product in products)
+            {
+                if (product.Name == productName) return product;
+            }
+
+            return null;
+        }
+      
+
+    }
+}
