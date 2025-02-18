de23f66bed44907e09ae2fd32a1fc0c808bc8dc2
Ruba <s12028675@stu.najah.edu>
2025-02-19 01:35:50 +0200

    Create Product Class

---
+++ ProductManagement/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INVMS.ProductManagement
+{
+    internal class Product
+    {
+        public string Name { set; get; } = string.Empty;
+        public int Quantity {  set; get; }
+        private double price;
+        public double Price
+        {
+            get {  return price; }
+            set { price = value > 0? value: 0; }
+        }
+        public Product(string name, int quantitiy, double price)
+        {
+            Name = name;
+            Quantity = quantitiy;
+            Price = price;
+        }
+        public override string ToString()
+        {
+            return $"| {Name.PadRight(20)} | {Price.ToString("0.00").PadRight(8)} | {Quantity.ToString().PadRight(8)} |";
+        }
+
+    }
+}
