99637db179af464c72663ec2d49b51cbb4a9a0d8
Ruba <s12028675@stu.najah.edu>
2025-02-19 01:36:29 +0200

    Add Generic method to read valid int or double number

---
+++ ProductManagement/Utilities.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INVMS.ProductManagement
+{
+    internal class Utilities <T>
+    {
+
+        public delegate bool TryParseHandler(string? input, out T result);
+
+
+        public static T GetNumber(string message, TryParseHandler tph)
+        {
+            bool isValid = false;
+            T value ;
+            do
+            {
+                Console.WriteLine(message);
+                string? inputValue = Console.ReadLine();
+                isValid = tph(inputValue, out value);
+                if (! isValid)
+                {
+                    Console.WriteLine("!!! Invalid price value !!!");
+                }
+
+            } while (!isValid);
+            return value;
+
+        }
+    }
+}
