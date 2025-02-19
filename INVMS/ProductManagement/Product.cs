85fcea545f0f33c872b963d687c332f9684aabe3
Ruba <s12028675@stu.najah.edu>
2025-02-19 14:47:16 +0200

    Remove Unnecessary Spaces

--- INVMS/ProductManagement/Product.cs
+++ INVMS/ProductManagement/Product.cs
@@ -1,33 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace INVMS.ProductManagement
+﻿namespace INVMS.ProductManagement;
+internal class Product
 {
-    internal class Product
+    public string Name { set; get; } = string.Empty;
+    private double price;
+    public int Quantity {  set; get; }
+
+    public double Price
     {
-        public string Name { set; get; } = string.Empty;
-        public int Quantity {  set; get; }
+        get {  return price; }
+        set { price = value > 0? value: 0; }
+    }
 
-        private double price;
-        public double Price
-        {
-            get {  return price; }
-            set { price = value > 0? value: 0; }
-        }
-        public Product(string name, int quantitiy, double price)
-        {
-            Name = name;
-            Quantity = quantitiy;
-            Price = price;
-        }
+    public Product(string name, int quantitiy, double price)
+    {
+        Name = name;
+        Quantity = quantitiy;
+        Price = price;
+    }
 
-        public override string ToString()
-        {
-            return $"| {Name.PadRight(20)} | {Price.ToString("0.00").PadRight(8)} | {Quantity.ToString().PadRight(8)} |";
-        }
-
+    public override string ToString()
+    {
+        return $"| {Name.PadRight(20)} | {Price.ToString("0.00").PadRight(8)} | {Quantity.ToString().PadRight(8)} |";
     }
 }
--- INVMS/ProductManagement/Utilities.cs
+++ INVMS/ProductManagement/Utilities.cs
@@ -1,27 +1,22 @@
-﻿namespace INVMS.ProductManagement
+﻿namespace INVMS.ProductManagement;
+internal class Utilities <T>
 {
-    internal class Utilities <T>
+    public delegate bool TryParseHandler(string? input, out T result);
+    public static T GetNumber(string message, TryParseHandler tph)
     {
-
-        public delegate bool TryParseHandler(string? input, out T result);
-
-        public static T GetNumber(string message, TryParseHandler tph)
+        bool isValid = false;
+        T value ;
+        do
         {
-            bool isValid = false;
-            T value ;
-            do
+            Console.WriteLine(message);
+            string? inputValue = Console.ReadLine();
+            isValid = tph(inputValue, out value);
+            if (! isValid)
             {
-                Console.WriteLine(message);
-                string? inputValue = Console.ReadLine();
-                isValid = tph(inputValue, out value);
-                if (! isValid)
-                {
-                    Console.WriteLine("!!! Invalid number value !!!");
-                }
+                Console.WriteLine("!!! Invalid number value !!!");
+            }
 
-            } while (!isValid);
-            return value;
-
-        }
+        } while (!isValid);
+        return value;
     }
 }
